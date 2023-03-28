d9a44840d6c787f1aefa6daeb8e6a74d66ceefdf
Scott Fuston <scott.fuston@grandpad.net>
2023-03-28 13:07:49 -0400

    moves DEV env check inside the if Props isEmpty block so it only happens once

--- src/main/java/com/qa/utils/DriverManager.java
+++ src/main/java/com/qa/utils/DriverManager.java
@@ -31,29 +31,28 @@ public class DriverManager {
 
         if(driver == null){
             try{
-
                 utils.log().info("initializing Appium driver");
+
                 switch(params.getPlatformName()){
                     case "Android":
-                    //    saucelabs
                           driver = new AndroidDriver(url, new CapabilitiesManager().getCaps());
-
-                          //local
-                     //     driver = new AndroidDriver(new ServerManager().getServer().getUrl(), new CapabilitiesManager().getCaps());
                         break;
                     case "iOS":
                           driver = new IOSDriver(new ServerManager().getServer().getUrl(), new CapabilitiesManager().getCaps());
-                     //   driver = new IOSDriver(url, new CapabilitiesManager().getCaps());
                         break;
                 }
+
                 if(driver == null){
                     throw new Exception("driver is null. ABORT!!!");
                 }
+
                 utils.log().info("Driver is initialized");
+
                 DriverManager.driver.set(driver);
             } catch (IOException e) {
-                e.printStackTrace();
                 utils.log().fatal("Driver initialization failure. ABORT !!!!" + e.toString());
+
+                e.printStackTrace();
                 throw e;
             }
         }
