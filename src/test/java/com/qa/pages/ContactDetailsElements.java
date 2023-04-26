4ec0be18894816ed29fdcca42d1d6bf783c3bbbc
Scott Fuston <scott.fuston@grandpad.net>
2023-04-26 11:09:11 -0400

    merge conflicts

--- src/test/java/com/qa/pages/ContactDetailsElements.java
+++ src/test/java/com/qa/pages/ContactDetailsElements.java
@@ -8,13 +8,13 @@ import java.io.IOException;
 
 
 public class ContactDetailsElements extends BasePage {
-
     public ContactDetailsElements() throws IOException {
     }
 
     @AndroidFindBy(xpath = "//android.widget.FrameLayout[@content-desc=\"Contacts\"]/android.view.ViewGroup/android.widget.TextView")
     @iOSXCUITFindBy(accessibility = "Contacts")
     public WebElement contactsBtn;
+
     @AndroidFindBy( xpath= "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/androidx.recyclerview.widget.RecyclerView/android.widget.FrameLayout[1]/android.view.ViewGroup")
     @iOSXCUITFindBy(iOSClassChain = "**/XCUIElementTypeWindow/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeTable/XCUIElementTypeCell[1]")
     public WebElement contact;
@@ -56,7 +56,4 @@ public class ContactDetailsElements extends BasePage {
 
     @AndroidFindBy(xpath = "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.ScrollView/androidx.recyclerview.widget.RecyclerView/android.widget.FrameLayout[2]/android.view.ViewGroup")
     public WebElement contactPost;
-
-
-
 }
